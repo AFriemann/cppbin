cf213f580ab6f0ad52fc51d147d7ed35353ec018
Aljosha Friemann <aljosha.friemann@gmail.com>
2016-12-30 05:13:10 +0100

    added automatic testing when running make

---
+++ test/iterators/FileIterator.cpp
@@ -0,0 +1,15 @@
+/* FileIterator.cpp
+ * -*- coding: utf-8 -*-
+ *
+ */
+
+#include "catch.hpp"
+
+SCENARIO( "foobar" ) {
+    // TODO
+    GIVEN( "foo foo bar" ) {
+        REQUIRE( true );
+    }
+}
+
+// vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+/* main.cpp
+ * -*- coding: utf-8 -*-
+ *
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+// vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
